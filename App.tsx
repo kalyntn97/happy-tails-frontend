0fffdce921aa84871d3024f136afe957c754ef76
kalyntn97 <kalyntn@gmail.com>
2023-12-06 16:26:31 -0500

    use ngrok to host backend server and make it accessible to front end app

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native'
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+//context
+import { AuthProvider, useAuth } from './context/AuthContext'
 //components
 import HomeScreen from './screens/HomeScreen'
 import SettingsScreen from './screens/SettingsScreen'
@@ -11,22 +13,41 @@ import LoginScreen from './screens/LoginScreen'
 import PetsScreen from './screens/PetsScreen'
 
 const App: React.FC = () => {
-  const Tab = createBottomTabNavigator()
 
   return (
+    <AuthProvider>
+      <Layout></Layout>
+    </AuthProvider>
+  )
+}
+
+export const Layout = () => {
+  const Tab = createBottomTabNavigator()
+  const { authState, onLogout } = useAuth()
+  return ( 
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Pets' component={PetsScreen} />
+        {authState?.authenticated ? (
+          <>
+            <Tab.Screen name='Home' component={HomeScreen} />
+            <Tab.Screen name='Pets' component={PetsScreen} />
+            <Tab.Screen name='Settings' component={SettingsScreen} />
+          </>
+        ) : (
+          <>
+            <Tab.Screen name='Home' component={HomeScreen} />
+            <Tab.Screen name='Login' component={LoginScreen} />
+          </>
+        )}
+    
         
-        <Tab.Screen name='Settings' component={SettingsScreen} />
-        <Tab.Screen name='Login' component={LoginScreen} />
 
       </Tab.Navigator>
     </NavigationContainer>
   )
 }
 
+export default App
 /* const styles  = StyleSheet.create({
   container: {
     flex: 1,
@@ -36,4 +57,3 @@ const App: React.FC = () => {
   },
 }); */
 
-export default App
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,11 +1,68 @@
-import { View, Text } from "react-native"
+//npm modules
+import { useState } from 'react'
+import { View, StyleSheet, TextInput, Button } from 'react-native'
+//context
+import { useAuth } from '../context/AuthContext'
 
 const LoginScreen = () => {
-  return ( 
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Login!</Text>
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const { onLogin, onRegister } = useAuth()
+
+  const login = async () => {
+    const result = await onLogin!(username, password)
+    if (result && result.error) {
+      alert(result.status)
+    }
+  }
+
+  const register = async () => {
+    const result = await onRegister!(username, password)
+    if (result && result.error) {
+      alert(result.msg)
+    } else {
+      login()
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='Username' 
+          onChangeText={(text: string) => setUserName(text)} 
+          value={username} 
+          autoCapitalize='none'
+        />
+        <TextInput 
+          style={styles.input} 
+          placeholder='Password' 
+          onChangeText={(text: string) => setPassword(text)} 
+          value={password} 
+          secureTextEntry={true}
+        />
+        <Button onPress={login} title='Sign in' />
+        <Button onPress={register} title='Create Account' />
+      </View>
     </View>
   )
 }
- 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+  },
+})
+
 export default LoginScreen
