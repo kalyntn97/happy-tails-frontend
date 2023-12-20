5c2b475112cda73c38bf77cb5a3517310ea15f05
kalyntn97 <kalyntn@gmail.com>
2023-12-19 23:30:00 -0500

    add pet icons and edit layout of pet card

--- components/PetCard.tsx
+++ components/PetCard.tsx
@@ -1,10 +1,17 @@
 //npm modules
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Image, ImageSourcePropType } from "react-native"
+//types
+import { Pet } from "../api/petsService"
 //styles
 import { Buttons, Spacing, Forms, Typography, Colors } from '../styles'
+interface PetCardProps {
+  pet: Pet;
+  currCard: number;
+  idx: number;
+}
 
-const PetCard = ({ pet, currCard, idx }) => {
-  const getStyle = (currCard, idx) => {
+const PetCard: React.FC<PetCardProps> = ({ pet, currCard, idx }) => {
+  const getStyle = (currCard: number, idx: number) => {
     if (currCard === 0) {
       return styles.base
     } else if (currCard === idx) {
@@ -15,17 +22,40 @@ const PetCard = ({ pet, currCard, idx }) => {
       return {...styles.baseSmall, ...styles.left}
     }
   }
+  const dynamicStyle = getStyle(currCard, idx)
+
+  const getIconSource = (species: string): ImageSourcePropType => {
+    switch (species) {
+      case 'dog':
+        return require('../assets/icons/dog.png')
+      case 'cat':
+        return require('../assets/icons/cat.png')
+      case 'bird':
+        return require('../assets/icons/bird.png')
+      case 'others':
+        return require('../assets/icons/animal.png')
+    }
+  }
+  const iconSource = getIconSource(pet.species)
 
   return ( 
-    <View style={getStyle(currCard, idx)}>
-      <Text style={styles.petName}>{pet.name}</Text>
-      <Text style={styles.label}>Age:
-        <Text style={styles.body}> {pet.age}</Text>
-      </Text>
-      <Text style={styles.label}>Age:
-        <Text style={styles.body}> {pet.breed}</Text>
-      </Text>
-      
+    <View style={dynamicStyle}>
+      <View style={styles.nameContainer}>
+        <Image style={styles.icon} source={iconSource} />
+        <Text style={styles.petName}>{pet.name}</Text>
+      </View>
+
+      <View style={styles.detailsContainer}>
+        <Image style={styles.petPhoto} source={pet.photo}/>
+        <View style={styles.petInfo}>
+          <Text style={styles.label}>Age:
+            <Text style={styles.body}> {pet.age}</Text>
+          </Text>
+          <Text style={styles.label}>Breed:
+            <Text style={styles.body}> {pet.breed}</Text>
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
@@ -35,18 +65,21 @@ const styles = StyleSheet.create({
     ...Forms.card,
     width: '90%',
     height: '90%',
+    justifyContent: 'flex-start',
     backgroundColor: Colors.lightestPink
   },
   baseSmall: {
     ...Forms.card,
     width: '85%',
     height: '85%',
+    backgroundColor: Colors.lightestPink,
+    opacity: .5
   },
   focused: {
-    transform: [{translateX: -320}]
+    transform: [{translateX: -300}]
   },
   left: {
-    transform: [{translateX: -320}],
+    transform: [{translateX: -300}],
   },
   right: {
     transform: [{translateX: 300}]
@@ -60,6 +93,30 @@ const styles = StyleSheet.create({
   body: {
     ...Typography.smallBody
   },
+  icon: {
+    width: 80,
+    height: 80,
+    // position: 'absolute',
+    // top: 0,
+    // left: 0,
+  },
+  nameContainer: {
+    ...Spacing.flexRow,
+    width: '90%',
+    justifyContent: 'flex-start'
+  },
+  detailsContainer: {
+    ...Spacing.flexRow,
+    width: '90%'
+  },
+  petInfo: {
+    width: '60%'
+  },
+  petPhoto: {
+    width: '40%',
+    borderRadius: 8,
+    margin: 5
+  }
 })
  
 export default PetCard
