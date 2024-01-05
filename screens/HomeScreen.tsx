9400261587339da3f241e362e62561b7d866f4df
kalyntn97 <kalyntn@gmail.com>
2024-01-05 18:55:35 -0500

    add pagination to homescreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 //npm modules
-import { View, Text, Pressable, TouchableOpacity, StyleSheet } from "react-native"
+import { useRef, useState } from "react"
+import { View, Text, Pressable, TouchableOpacity, StyleSheet, useWindowDimensions, ScrollView, Image, ImageStyle } from "react-native"
 import LottieView from 'lottie-react-native'
 //context
 import { useAuth } from "../context/AuthContext"
@@ -9,16 +10,30 @@ import { Buttons, Spacing, Forms, Typography, Colors } from '../styles'
 const HomeScreen: React.FC = ({ navigation }) => {
   const { authState } = useAuth()
 
+  const windowHeight = useWindowDimensions().height
+  const scrollViewRef = useRef<ScrollView>(null)
   
+  const scrollToNext = (pageNum: number) => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({ y: pageNum * windowHeight, animated: true })
+    }
+  }
 
   return ( 
-    <>
+    <ScrollView
+      ref={scrollViewRef}
+      pagingEnabled
+      showsVerticalScrollIndicator={false}
+      scrollEventThrottle={200}
+      decelerationRate="fast"
+    >
       {authState?.authenticated ? (
         <View style={styles.container}>
           
         </View>
       ) : (
-        <View style={styles.container}>
+      <>
+        <View style={[styles.screen, { height: windowHeight }]}>
            <LottieView source={require('../assets/animations/happy.json')} autoPlay loop style={styles.happyAnimation} />
            <View style={styles.headers}>
               <Text style={styles.mainHeader}>
@@ -29,21 +44,39 @@ const HomeScreen: React.FC = ({ navigation }) => {
               <Text style={styles.subHeader}>Ready to start a new journey with your furry friends?</Text>
            </View>
           <TouchableOpacity onPress={() => navigation.navigate('User', { screen: 'Login' })} style={styles.mainBtn}>
-            <Text style={styles.btnText}>Get Started</Text>
+            <Text style={[styles.btnText, { color: Colors.lightestPink }]}>Get Started</Text>
           </TouchableOpacity>
-          {/* <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-            <Text>Register</Text>
-          </TouchableOpacity> */}
+          <TouchableOpacity style={styles.link} onPress={() => scrollToNext(1)}>
+            <LottieView source={require('../assets/animations/downArrow.json')} autoPlay loop style={styles.icon}/>
+          </TouchableOpacity>
         </View>
+
+        <View style={[styles.screen, { height: windowHeight, backgroundColor: Colors.lightPink }]}>
+          <TouchableOpacity style={styles.link} onPress={() => scrollToNext(2)}>
+            <LottieView source={require('../assets/animations/downArrow.json')} autoPlay loop style={styles.icon}/>
+          </TouchableOpacity>
+        </View>
+
+        <View style={[styles.screen, { height: windowHeight, backgroundColor: Colors.yellow }]}>
+          <TouchableOpacity style={styles.link} onPress={() => scrollToNext(0)}>
+            <LottieView source={require('../assets/animations/downArrow.json')} autoPlay loop style={styles.icon}/>
+          </TouchableOpacity>
+        </View>
+
+      </>
       )}
-    </>
-    // </HomeStack.Navigator>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     ...Spacing.fullWH,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  screen: {
+    width: '100%',
     alignItems: 'center',
     backgroundColor: 'white',
   },
@@ -71,13 +104,20 @@ const styles = StyleSheet.create({
     width: '80%',
     height: 50,
     backgroundColor: Colors.pink,
-    marginTop: 'auto',
-    marginBottom: 80,
+    marginTop: 10,
+    
   },
   btnText: {
     ...Buttons.buttonText,
     fontSize: 20,
-    color: Colors.lightestPink
+  },
+  link: {
+    ...Buttons.base,
+    marginTop: 'auto'
+  },
+  icon: {
+    width: 100, 
+    height: 100,
   }
 })
  
